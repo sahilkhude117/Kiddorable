6cf9a5c4ae6a55db3bcea4107716d92864eed4f2
sahilkhude117 <sahilkhude11@gmail.com>
2025-02-05 02:58:25 +0530

    types

---
+++ src/app/lib/types.ts
@@ -0,0 +1,12 @@
+// lib/types.ts
+export type Product = {
+    id: number
+    name: string
+    slug: string
+    price: number
+    originalPrice: number
+    discountPercentage: number
+    ageGroup: string
+    previewUrl: string
+    shortDescription: string
+  }
