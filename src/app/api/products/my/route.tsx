05ec07298fc0d90e680d6e61128f2809653cb5d7
sahilkhude117 <sahilkhude11@gmail.com>
2025-02-07 23:20:56 +0530

    my products

---
+++ src/app/api/products/my/route.tsx
@@ -0,0 +1,48 @@
+import { NEXT_AUTH_CONFIG } from "@/lib/auth"
+import prisma from "@/lib/db";
+import { getServerSession } from "next-auth"
+import { NextResponse } from "next/server";
+
+
+export const GET = async () => {
+    const session = await getServerSession(NEXT_AUTH_CONFIG);
+    const user = session?.user;
+
+    try{
+        const myProducts = await prisma.purchase.findMany({
+            where:{
+                user: {
+                    email: user?.email
+                }
+            },
+            select:{
+                purchasedAt: true,
+                product:{
+                    select: {
+                        id: true,
+                        title: true,
+                        thumbnailImage: true,
+                        driveLink: true,
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json({
+            myProducts
+        },{
+            status:200
+        })
+    } catch(e) {
+        console.log('Error', e);
+    return NextResponse.json(
+      {
+        error: 'Internal Server Error',
+        details: e,
+      },
+      {
+        status: 500,
+      }
+    );
+    }   
+}
