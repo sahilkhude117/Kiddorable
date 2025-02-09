b00b2b23c69975a2012d204229f8793ca8c22a05
sahilkhude117 <sahilkhude11@gmail.com>
2025-02-09 22:52:16 +0530

    create order

--- src/app/api/payment/create-order/route.tsx
+++ src/app/api/payment/create-order/route.tsx
@@ -1,27 +1,60 @@
-import { NextResponse } from 'next/server';
-import Razorpay from 'razorpay';
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
+import Razorpay from "razorpay";
 
-export async function POST(req: Request) {
-    
-    const razorpay = new Razorpay({
-        key_id: process.env.RAZORPAY_KEY_ID || "default",
-        key_secret: process.env.RAZORPAY_SECRET_KEY || "default",
-    })
-
-    const body = await req.json();
-    const { amount } = body;
-
-    const options = {
-        amount: amount * 100,
-        currency: 'INR',
-        receipt: `order_${Date.now()}`,
-    }
-
+export const POST = async (req: Request) => {
     try {
-        const order = await razorpay.orders.create(options);
-        return NextResponse.json({ order }, { status: 200 });
-    } catch (error) {
-        console.error('Error creating order:', error);
-        return NextResponse.json({ error: 'Failed to create order' }, { status: 500 });
+
+        const razorpay = new Razorpay({
+            key_id: process.env.RAZORPAY_KEY_ID || 'default',
+            key_secret: process.env.RAZORPAY_KEY_SECRET || 'default',
+        })
+        // parse request body
+        const body = await req.json();
+        const { 
+            amount,
+            currency,
+            receipt,
+            userId,
+            email,
+            name,
+            productId
+        } = body;
+
+        // validate input
+        if (!amount || !currency || !receipt || !email || !name || !productId) {
+            return NextResponse.json({ error: 'Invalid input' }, { status: 400 });
+        }
+
+        //Create Razorpay order
+        const order = await razorpay.orders.create({
+            amount: amount * 100,
+            currency,
+            receipt,
+        })
+
+        await prisma.purchase.create({
+            data:{
+                orderId: order.id,
+                userId: userId || null,
+                productId,
+                status: 'PENDING',
+                email,
+                name,
+            }
+        });
+
+        return NextResponse.json({
+            order,
+        },{
+            status: 200
+        })
+    } catch(e){
+        console.error(e);
+        return NextResponse.json({
+            error: 'Failed to create order'
+        },{
+            status : 500
+        })
     }
-}
+};
