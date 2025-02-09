5a59b506a27b8fcfff0eb202e9fe7206753193e9
sahilkhude117 <sahilkhude11@gmail.com>
2025-02-09 22:55:24 +0530

    signin

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -6,10 +6,67 @@ import Link from 'next/link'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
-import { Apple, ArrowRight } from 'lucide-react'
-import { TrustBadges } from '@/app/components/shared/TrustBadges'
+import { Apple, ArrowRight, Eye, EyeOff } from 'lucide-react'
+
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { signIn } from 'next-auth/react'
 
 export default function SignupPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState(''); 
+  const [hiddenPassword, setHiddenPassword] = useState(true);  
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const toggleVisibility = () => {
+    setHiddenPassword(!hiddenPassword);
+  }
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    setLoading(true);
+    setError('');
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if(!emailRegex.test(email)){
+        setError('Invalid email address');
+        setLoading(false);
+        return;
+    }
+
+    if(!passwordRegex.test(password)){
+        setError('Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character');
+        setLoading(false);
+        return;
+    }
+
+    try {
+        const result = await signIn('credentials', {
+            redirect: false,
+            email,
+            password,
+            name,
+        });
+
+        if(result?.error){
+            setError('Invalid email or password');
+            setLoading(false);
+            return;
+        } else {
+            router.push('/')
+        }
+    } catch (err) {
+        setError('Something went wrong. Please try again.');
+    } finally {
+        setLoading(false);
+    }
+  }
+
   return (
     <div className="min-h-screen pb-24">
       <div className="max-w-md mx-auto px-4 py-12">
@@ -36,9 +93,16 @@ export default function SignupPage() {
                     Full Name
                   </label>
                   <Input
+                    id='name'
                     type="text"
+                    autoComplete='text'
                     className="mt-1 focus-visible:ring-[#34C759]"
-                    placeholder="John Doe"
+                    placeholder="Tony Stark"
+                    value={name}
+                    onChange={(e) => {
+                      setError('')
+                      setName(e.target.value)
+                    }}
                   />
                 </div>
 
@@ -47,9 +111,18 @@ export default function SignupPage() {
                     Email Address
                   </label>
                   <Input
+                    id='email'
+                    name='email'
                     type="email"
+                    autoComplete='email'
+                    required
                     className="mt-1 focus-visible:ring-[#34C759]"
-                    placeholder="your@email.com"
+                    placeholder="@tonystark.in"
+                    value={email}
+                    onChange={(e)=> {
+                      setError('')
+                      setEmail(e.target.value)
+                    }}
                   />
                 </div>
 
@@ -58,19 +131,55 @@ export default function SignupPage() {
                     Password
                   </label>
                   <Input
-                    type="password"
+                    id='password'
+                    name='password'
+                    type={ hiddenPassword ? 'password' : 'text'}
+                    autoComplete='password'
+                    required
                     className="mt-1 focus-visible:ring-[#34C759]"
-                    placeholder="••••••••"
+                    placeholder="••••••••••••••••"
+                    value={password}
+                    onChange={(e) => {
+                      setError('')
+                      setPassword(e.target.value)
+                    }}
                   />
                 </div>
+                <div
+                    onClick={toggleVisibility}
+                    className="absolute right-20 top-[380] flex items-center cursor-pointer lg:right-[600]"
+                >
+                    {hiddenPassword ? <EyeOff size={20}/> : <Eye size={20}/> }
+                </div>
               </div>
+              {/* Error Message */}
+              {error && (
+                  <div className="text-red-500 text-sm text-center">
+                      {error}
+                  </div>
+              )}
 
               <Button
+                type='submit'
+                disabled={loading}
                 className="w-full bg-gradient-to-r from-[#FFD700] to-[#FFB700] hover:from-[#FFB700] hover:to-[#FF9500] text-[#2A5C8F] py-5 font-bold"
                 size="lg"
+                onClick={handleSubmit}
               >
-                Create Account
-                <ArrowRight className="ml-2 h-4 w-4" />
+                {loading ? (
+                    <span className="flex items-center">
+                    <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                    Creating Acount...
+                  </span>
+                ) : (
+                  <div className='flex justify-center'>
+                    Create Account
+                    <ArrowRight className="ml-2 h-4 w-4" />
+                  </div>
+                )}
               </Button>
             </form>
 
@@ -106,7 +215,7 @@ export default function SignupPage() {
           <p className="mt-8 text-center text-[#6B7280]">
             Already have an account?{' '}
             <Link
-              href="/login"
+              href="/auth/login"
               className="text-[#34C759] font-medium hover:underline"
             >
               Sign in
@@ -124,8 +233,6 @@ export default function SignupPage() {
             </Link>
           </p>
         </motion.div>
-
-        <TrustBadges />
       </div>
     </div>
   )
