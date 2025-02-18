f962c3a5f8cd7b38aac40a629f7c2d5c793faaef
sahilkhude117 <sahilkhude11@gmail.com>
2025-02-18 19:39:04 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./providers";
 import { UserProvider } from "@/contexts/userContext";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
         <AuthProvider>
           <UserProvider>
             {children}
+            <Analytics />
           </UserProvider>
         </AuthProvider>
       </body>
